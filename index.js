b941ebd8ef38610e333c47a4eb298e1b62e8a0a1
Braden M. Kelley <redbmk@gmail.com>
2018-10-27 17:34:24 -0700

    add logout route

--- index.js
+++ index.js
@@ -30,7 +30,17 @@ app.use(oidc.router)
 
 app.use('/register', require('./routes/register'))
 
-// @TODO add logout route
+app.get('/logout', (req, res) => {
+  if (req.userContext) {
+    const idToken = req.userContext.tokens.id_token
+    const to = encodeURI(process.env.HOST_URL)
+    const params = `id_token_hint=${idToken}&post_logout_redirect_uri=${to}`
+    req.logout()
+    res.redirect(`${process.env.OKTA_ORG_URL}/oauth2/default/v1/logout?${params}`)
+  } else {
+    res.redirect('/')
+  }
+})
 
 app.use('/', require('./routes/index'))
 
