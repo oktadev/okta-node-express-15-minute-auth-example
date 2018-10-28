e4ac3ca73499a9705d641c46ce922f1b988b8b22
Braden M. Kelley <redbmk@gmail.com>
2018-10-27 18:07:22 -0700

    add links, protect home page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('index')
+  const { userContext } = req
+  res.render('index', { userContext })
 })
 
 module.exports = router
