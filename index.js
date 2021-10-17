ceb3e242071fe92d0b1b1254941b8a9e2d4689a1
deepu105 <d4udts@gmail.com>
2021-10-17 16:31:25 +0200

    update dependencies

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
 app.use(express.urlencoded({ extended: true }));
-app.use('/static', express.static('public'));
+app.use("/static", express.static("public"));
 
 app.use(
-  require('express-session')({
+  require("express-session")({
     secret: process.env.APP_SECRET,
     resave: true,
-    saveUninitialized: false
+    saveUninitialized: false,
   })
 );
-  
-const { ExpressOIDC } = require('@okta/oidc-middleware');
+
+const { ExpressOIDC } = require("@okta/oidc-middleware");
 const oidc = new ExpressOIDC({
   appBaseUrl: process.env.HOST_URL,
   issuer: `${process.env.OKTA_ORG_URL}/oauth2/default`,
   client_id: process.env.OKTA_CLIENT_ID,
   client_secret: process.env.OKTA_CLIENT_SECRET,
   redirect_uri: `${process.env.HOST_URL}/callback`,
-  scope: 'openid profile',
+  scope: "openid profile",
   routes: {
     loginCallback: {
-      path: '/callback'
+      path: "/callback",
     },
-  }
+  },
 });
-  
+
 app.use(oidc.router);
 
-app.use('/', require('./routes/index'));
-app.use('/register', require('./routes/register'));
+app.use("/", require("./routes/index"));
+app.use("/register", require("./routes/register"));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App listening on port ${port}`));
